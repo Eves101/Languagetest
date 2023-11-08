3945f352dc190175f9d07b92618f9ecd4c4f3c83
Evert <e.scherpenborg@prognotice.nl>
2023-11-08 08:36:58 +0100

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Lang = MauiApp1.Resources.Translations.Resource;
+namespace MauiApp1
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+
+			
+
+			if (count == 1)
+				CounterBtn.Text = $"Clicked {count} time";
+			else
+				CounterBtn.Text = $"Clicked {count} times";
+
+			SemanticScreenReader.Announce(CounterBtn.Text);
+
+			if(Application.Current is not null && Application.Current.MainPage is not null)
+			{
+				await Application.Current.MainPage.DisplayAlert("Alert", "You have (not) been alerted", Lang.MsgOk );
+
+			}
+		}
+	}
+
+}
